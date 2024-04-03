f3ce21559b0b14682995b01629ddc9a42ca516f3
jujpenabe <jujpenabe@unal.edu.co>
2024-04-03 18:43:50 -0500

    sin mente

--- crackedice/addons/EasyMenus/Scripts/main_menu_controller.gd
+++ crackedice/addons/EasyMenus/Scripts/main_menu_controller.gd
@@ -3,19 +3,19 @@ signal start_game_pressed
 
 @onready var start_game_button: Button = $%StartGameButton
 @onready var options_menu: Control = $%OptionsMenu
-@onready var content: Control = $%Content 
+@onready var content: Control = $%Content
 
 func _ready():
 	start_game_button.grab_focus()
 
 func quit():
 	get_tree().quit()
-	
+
 func open_options():
 	options_menu.show()
 	content.hide()
 	options_menu.on_open()
-	
+
 func close_options():
 	content.show();
 	start_game_button.grab_focus()
@@ -26,7 +26,6 @@ func _on_start_game_button_pressed():
 	emit_signal("start_game_pressed")
 	# TODO: Add option to skip intro
 	# Fade out and go to next scene
-	get_tree().change_scene_to_file("res://CrackedIce/Maps/Main/main.tscn")
-	
-	
-	
+	get_tree().change_scene_to_file("res://CrackedIce/Maps/Main/Main.tscn")
+
+
