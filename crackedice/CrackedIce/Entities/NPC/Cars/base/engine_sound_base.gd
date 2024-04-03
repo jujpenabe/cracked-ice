aac947d4fc0f48ad3c76bf20b078ea91f83415c9
Dani Bazurto <dbg1209@gmail.com>
2024-04-03 17:55:18 -0500

    Test
    Updated files and test

--- crackedice/CrackedIce/Entities/NPC/Cars/base/engine_sound_base.gd
+++ crackedice/CrackedIce/Entities/NPC/Cars/base/engine_sound_base.gd
@@ -4,5 +4,5 @@ extends AudioStreamPlayer3D
 @export var sample_rpm := 4000.0
 
 func _physics_process(delta):
-	pitch_scale = vehicle.motor_rpm * 0.8 / sample_rpm
+	pitch_scale = vehicle.motor_rpm * 0.7 / sample_rpm
 	volume_db = linear_to_db((vehicle.throttle_amount * 0.4) + 0.6)
