b37132fb1423d37a51b7a65f2c4da88aa3a98719
jujpenabe <jujpenabe@unal.edu.co>
2024-04-12 17:44:56 -0500

    Revert "Fixed House1 texture and model, some car tweaks"
    This reverts commit 6847bbb704d0026fb8d1373c4f0fbeeefa397de1.

--- crackedice/CrackedIce/Entities/NPC/Cars/base/engine_sound_base.gd
+++ crackedice/CrackedIce/Entities/NPC/Cars/base/engine_sound_base.gd
@@ -5,4 +5,4 @@ extends AudioStreamPlayer3D
 
 func _physics_process(delta):
 	pitch_scale = vehicle.motor_rpm * 0.7 / sample_rpm
-	volume_db = linear_to_db((pow(vehicle.throttle_amount, 2) * 0.25) + 0.5) # max value is expected at 0.75
+	volume_db = linear_to_db((vehicle.throttle_amount * 0.4) + 0.6)
